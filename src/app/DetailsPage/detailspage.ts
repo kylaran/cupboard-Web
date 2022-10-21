09a17fc662d13d2d72aaffbe804489afd2bdcdae
DESKTOP-HAERTJK\ilysh <ilyshka19890307@gmail.com>
2022-10-21 17:13:09 +0300

    add new page

---
+++ src/app/DetailsPage/detailspage.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+import { Reviews} from "src/services/objectViews"
+
+
+@Component({
+  selector: 'app-detailspage',
+  templateUrl: 'detailspage.html',
+  styleUrls: ['detailspage.css']
+})
+export class DetailsPage {
+modal = false
+  constructor() { }
+
+
+}
